32eddf3be6fc5649e0f5283e89937d8e48d2ba68
fahad alshoaibi <tut72113@temple.edu>
2026-09-04 13:05:35 -0400

    added withdrawl for branch a

--- BankAccount.java
+++ BankAccount.java
@@ -9,11 +9,13 @@ public void deposit(double amount) {
 // TODO: add the amount to the balance
 }
 public boolean withdraw(double amount) {
-// TODO:
-// Withdraw the requested amount if sufficient funds are available.
-// Return true if the withdrawal succeeds.
-// Return false otherwise. return false;
-return false;
+    if (balance - amount >= 50) {
+        balance -= amount;
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 public String getAccountSummary() {
 return accountHolder + ": $" + balance;

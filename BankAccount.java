30d048445c1101bda4ec8112d1dcbede86869f43
fahad alshoaibi <tut72113@temple.edu>
2026-09-04 13:23:04 -0400

    final commit with siruation that suits both

--- BankAccount.java
+++ BankAccount.java
@@ -9,8 +9,8 @@ public void deposit(double amount) {
 // TODO: add the amount to the balance
 }
 public boolean withdraw(double amount) {
-    if (balance - amount >= 50) {
-        balance -= amount;
+    if (balance - amount >= 52) {
+        balance = balance - amount - 2;
         return true;
     }
     else{
